c2eb0e01d970d77951940671ce2103c44c5ab931
Adrian Tamas <adrian.tamas@endava.com>
2019-05-20 10:29:47 +0300

    update setup

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 
 from version import __version__
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='reportportal-behave-client',
@@ -9,8 +11,16 @@ setup(
     package_data={'': ['']},
     version=__version__,
     description='ReportPortal integration client lib',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Adrian Tamas',
     author_email='adi.tamas@outlook.com',
     license='MIT',
-    python_requires='>=3'
+    python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/Adrian-Tamas/reportportal-behave-integration-client-lib",
 )
